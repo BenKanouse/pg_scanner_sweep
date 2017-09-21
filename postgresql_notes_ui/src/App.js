7ffaa75098e16b1a1fc49517b5f2914cca6847d4
BenKanouse <kanobt61@gmail.com>
2017-09-21 01:40:34 -0400

    Update stat table to return real values
    The stat table now returns real values from the Sinatra app. It could
    use a bit more formatting but we are getting pretty close. Side note I
    might have introduced a security flaw with cross sign scripting.

--- postgresql_notes_ui/src/App.js
+++ postgresql_notes_ui/src/App.js
@@ -13,9 +13,6 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h2>Welcome to React</h2>
           </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
         <TableWithStat/>
         </div>
       </MuiThemeProvider>
--- postgresql_notes_ui/src/TableWithStat.js
+++ postgresql_notes_ui/src/TableWithStat.js
@@ -9,45 +9,58 @@ import {
 } from 'material-ui/Table';
 
 class TableWithStat extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: [{usename: 'username'}]
+      }
+   }
+
+  componentDidMount() {
+    var component = this;
+    console.log('componentDidMount');
+    fetch('http://localhost:4567/pg_activity_stats')
+    .then( (response) => {
+      return response.json()
+    })
+    .then( (myJson) => {
+      component.setState({data: myJson})
+    })
+  }
+
   render() {
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHeaderColumn>ID</TableHeaderColumn>
-            <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Status</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableRowColumn>1</TableRowColumn>
-            <TableRowColumn>John Smith</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>2</TableRowColumn>
-            <TableRowColumn>Randal White</TableRowColumn>
-            <TableRowColumn>Unemployed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>3</TableRowColumn>
-            <TableRowColumn>Stephanie Sanders</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>4</TableRowColumn>
-            <TableRowColumn>Steve Brown</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>5</TableRowColumn>
-            <TableRowColumn>Christopher Nolan</TableRowColumn>
-            <TableRowColumn>Unemployed</TableRowColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
-    );
+  var json = this.state.data
+  var tableHeaders = [];
+  var headers = [];
+  for (var header in json[0]) {
+    if (header !== 'client_addr') {
+      headers.push(header);
+      tableHeaders.push(<TableHeaderColumn key= { 'header_' + header} > {header} </TableHeaderColumn>);
+    };
+  };
+
+  var tableRows = [];
+  for (var rowIndex = 0; rowIndex < json.length; rowIndex++) {
+    var tableRow = [];
+    for (var columnIndex = 0; columnIndex < headers.length; columnIndex++) {
+      tableRow.push(<TableRowColumn key= { 'row_column_' + rowIndex + '_' + columnIndex }> {json[rowIndex][headers[columnIndex]]} </TableRowColumn>);
+    }
+    tableRows.push(<TableRow key= { 'row_' + rowIndex }> {tableRow} </TableRow>);
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          { tableHeaders }
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+          { tableRows }
+      </TableBody>
+    </Table>
+  );
+
   }
 }
 export default TableWithStat;
